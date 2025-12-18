f06cbdd08780c0c8b0853d44926267c557cc6e91
Ben-dev185 <ibalaloundoubendji@gmail.com>
2025-12-18 10:47:34 +0100

    modification de la methode de hachage pour utiliser BCrypt

--- src/main/java/com/gestion/ulka_stock/util/PasswordHashGenerator.java
+++ src/main/java/com/gestion/ulka_stock/util/PasswordHashGenerator.java
@@ -4,10 +4,11 @@ public class PasswordHashGenerator {
 
     public static void main(String[] args) {
 
-        System.out.println("admin123  -> "
-                + PasswordHasher.hashPassword("admin123"));
+        System.out.println("admin123 -> "
+                + PasswordHasher.hash("admin123"));
 
-        System.out.println("user123   -> "
-                + PasswordHasher.hashPassword("user123"));
+        System.out.println("user123  -> "
+                + PasswordHasher.hash("user123"));
     }
 }
+
